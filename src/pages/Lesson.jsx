e0b94503b746a9de5aa7fafafe43f68a90e73ab1
Alun <50495222+41un@users.noreply.github.com>
2023-09-22 09:29:32 +0800

    added success animation

--- src/pages/Lesson.jsx
+++ src/pages/Lesson.jsx
@@ -5,7 +5,8 @@ import Vimeo from '@u-wave/react-vimeo';
 import Confetti from 'react-confetti';
 import courses from '../components/courses';
 import { Button, Radio, RadioGroup, FormControlLabel, FormControl, Paper, Typography, CircularProgress, Modal } from '@mui/material';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import Lottie from "lottie-react";
+import successAnimation from '../components/animations/successAnimation.json'
 
 function Lesson() {
     const { courseId, lessonId } = useParams();
@@ -21,31 +22,37 @@ function Lesson() {
     });
     const [progress, setProgress] = useState(0);
     const [showNextButton, setShowNextButton] = useState(false);
+    const [submittedAnswers, setSubmittedAnswers] = useState(null);
+    const [lottieSize, setLottieSize] = useState({ width: 400, height: 400 });
+
 
     const course = courses.find(c => c.id === parseInt(courseId, 10));
     const lesson = course?.lessons.find(l => l.id === parseInt(lessonId, 10));
 
+
     useEffect(() => {
-      let progressTimer;
-  
-      if (correctCount === lesson.quiz.length && !showNextButton) {
-          progressTimer = setInterval(() => {
-              setProgress((prevProgress) => {
-                  if (prevProgress >= 100) {
-                      clearInterval(progressTimer);
-                      setShowNextButton(true);
-                      return 100;
-                  }
-                  return Math.min(prevProgress + 2, 100);
-              });
-          }, 100); 
-      }
-  
-      return () => {
-          clearInterval(progressTimer);
-      };
-  }, [correctCount, lesson.quiz.length, showNextButton]);
-  
+        let progressTimer;
+        const targetProgress = correctCount === lesson.quiz.length ? 100 : ((correctCount / lesson.quiz.length) * 100);
+        
+        progressTimer = setInterval(() => {
+            setProgress((prevProgress) => {
+                if (prevProgress >= targetProgress) {
+                    clearInterval(progressTimer);
+                    if (correctCount === lesson.quiz.length) {
+                        setShowNextButton(true);
+                    }
+                    return targetProgress;
+                }
+                return Math.min(prevProgress + 3, targetProgress);
+            });
+        }, 75);
+    
+        return () => {
+            clearInterval(progressTimer);
+        };
+    }, [correctCount, lesson.quiz.length, showNextButton]);
+    
+    
 
     useEffect(() => {
         const handleResize = () => {
@@ -58,6 +65,25 @@ function Lesson() {
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
     }, []);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setDimensions({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+    
+            if(window.innerWidth < 768) { // Assuming 768px is your breakpoint for mobile
+                setLottieSize({ width: 200, height: 200 }); // Or any size that fits your design
+            } else {
+                setLottieSize({ width: 400, height: 400 }); // Desktop size
+            }
+        };
+    
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    
 
     const handleOptionChange = (questionIndex, event) => {
         setAnswers(prev => {
@@ -79,6 +105,8 @@ function Lesson() {
         });
 
         setCorrectCount(count);
+        setSubmittedAnswers(answers);
+
         if (count === lesson.quiz.length) {
             setShowConfetti(true);
             setTimeout(() => setShowConfetti(false), 5000);
@@ -96,6 +124,8 @@ function Lesson() {
 
     const nextLessonId = lesson.id + 1;
     const nextLessonExists = course.lessons.some(l => l.id === nextLessonId);
+    const nextLesson = course.lessons.find(l => l.id === nextLessonId);
+
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', flexDirection: 'column', padding: '20px' }}>
@@ -113,7 +143,7 @@ function Lesson() {
                     )}
                     <Vimeo 
                         video={lesson.vimeoId.toString()}
-                        autoplay
+                        // autoplay
                         responsive={true}
                         title={false}
                         byline={false}
@@ -124,7 +154,16 @@ function Lesson() {
                 <div>
                     <Typography variant="h5" gutterBottom>Quiz</Typography>
                     {lesson.quiz.map((q, index) => (
-                        <div key={index}>
+                        <div 
+                            key={index} 
+                            style={{
+                                backgroundColor: submittedAnswers && submittedAnswers[index] === q.correctAnswer ? 'lightgreen' : 
+                                                 submittedAnswers && submittedAnswers[index] !== q.correctAnswer ? 'lightcoral' : 'transparent',
+                                padding: '10px',
+                                borderRadius: '5px',
+                                margin: '5px 0'
+                            }}
+                        >
                             <Typography variant="h6" gutterBottom>{q.question}</Typography>
                             <FormControl component="fieldset">
                                 <RadioGroup
@@ -167,30 +206,48 @@ function Lesson() {
                 <div style={{ backgroundColor: '#fff', padding: '20px', borderRadius: '10px' }}>
                     {correctCount === lesson.quiz.length ? (
                         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                            <CheckCircleIcon style={{ color: 'green', fontSize: '60px' }} />
+                        <Lottie 
+                            animationData={successAnimation} 
+                            loop={false}
+                            style={{ width: lottieSize.width, height: lottieSize.height }}
+                        />
                             <Typography variant="h4" style={{ color: 'green' }}>
                                 100%
                             </Typography>
                             <Typography variant="h6">
-                                Congratulations! You can proceed to the next lesson.
+                                Congratulations! You can proceed to {nextLessonExists ? `"${nextLesson?.title}"` : 'the next lesson'}.
                             </Typography>
                             {!showNextButton && <CircularProgress variant="determinate" value={progress} />}
                             {showNextButton && (
-                                <Link to={`/courses/${courseId}/lessons/${nextLessonId}`}>
-                                    <Button 
-                                        variant="contained" 
-                                        color="primary"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Next Lesson
-                                    </Button>
-                                </Link>
-                            )}
+                              <Link to={`/courses/${courseId}/lessons/${nextLessonId}`}>
+                                  <Button 
+                                      variant="contained" 
+                                      color="primary"
+                                      onClick={() => {
+                                          setShowModal(false);
+                                          setSubmittedAnswers(null); 
+                                      }}
+                                  >
+                                      Next Lesson
+                                  </Button>
+                              </Link>
+                          )}
+
                         </div>
                     ) : (
-                        <Typography>
-                            You got {correctCount} out of {lesson.quiz.length} questions correct.
-                        </Typography>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                            <Typography>
+                                You got {correctCount} out of {lesson.quiz.length} questions correct.
+                            </Typography>
+                            <Button 
+                                variant="contained" 
+                                color="secondary" 
+                                sx={{ marginTop: '20px' }} 
+                                onClick={() => setShowModal(false)}
+                            >
+                                Try Again
+                            </Button>
+                        </div>
                     )}
                 </div>
             </Modal>
